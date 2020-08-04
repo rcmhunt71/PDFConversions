0114187fb795b0917800bd609eee121bc140f5fd
rcmhunt71 <rcmhunt71@gmail.com>
2020-08-04 17:20:43 +0000

    Use Enum value rather than string for default value. Refactored name to improve readability

--- pdf_conversion/config/cli.py
+++ pdf_conversion/config/cli.py
@@ -7,7 +7,7 @@ class CommandLine:
 
     DEFAULT_DPI = 100
     DEFAULT_QUALITY = 90
-    TARGET_TYPE = 'webp'
+    TARGET_FORMAT = SupportedDocTypes.WEBP.value
 
     CONV_TYPES = dict([(doc_type.name, doc_type.value) for doc_type in SupportedDocTypes if
                        not doc_type.name.lower().startswith("not")])
@@ -18,14 +18,14 @@ class CommandLine:
                                  default=self.DEFAULT_DPI, type=int)
         self.parser.add_argument("-q", "--quality", help=f"Quality Factor [0, 100] (Default: {self.DEFAULT_QUALITY})",
                                  default=self.DEFAULT_QUALITY, type=int)
-        self.parser.add_argument("-t", "--type",
+        self.parser.add_argument("-f", "--format",
                                  help=f"Conversion Format. Supported Formats: "
                                       f"{', '.join(sorted(list(self.CONV_TYPES.values())))}"
-                                      f"  (Default: {self.TARGET_TYPE})",
-                                 default=self.TARGET_TYPE)
+                                      f"  (Default: {self.TARGET_FORMAT})",
+                                 default=self.TARGET_FORMAT)
         self.args = self.parser.parse_args()
-        self.args.doc_type = self._validate_doc_type()
+        self.args.doc_format = self._validate_doc_format_type()
 
-    def _validate_doc_type(self):
-        return (self.CONV_TYPES[self.args.type.lower()] if self.args.type.lower() in self.CONV_TYPES else
-                self.CONV_TYPES[CommandLine.TARGET_TYPE])
+    def _validate_doc_format_type(self):
+        return (self.CONV_TYPES[self.args.format.lower()] if self.args.format.lower() in self.CONV_TYPES else
+                self.CONV_TYPES[CommandLine.TARGET_FORMAT])
